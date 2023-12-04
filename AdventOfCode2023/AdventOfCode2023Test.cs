7fc0bfbec1a8733564ed02020723983acbaa1c5d
Fiorenzo Rutschmann <fiorenzo.rutschmann@otago.ac.nz>
2023-12-04 19:43:53 +1300

    Day 3 Part 1

--- AdventOfCode2023/AdventOfCode2023Test.cs
+++ AdventOfCode2023/AdventOfCode2023Test.cs
@@ -1,9 +1,18 @@
 using System.Text.RegularExpressions;
+using Xunit.Abstractions;
 
 namespace AdventOfCode2023
 {
     public class AdventOfCode2023Test
     {
+        private readonly ITestOutputHelper _log;
+
+        public AdventOfCode2023Test(ITestOutputHelper testOutputHelper)
+        {
+            _log = testOutputHelper;
+        }
+
+
         [Theory]
         [InlineData("./input/day1.txt", 55712)]
         public void Day1Part1(string input, int expected)
@@ -162,6 +171,132 @@ namespace AdventOfCode2023
             }
 
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("./input/day3test.txt", 4361)]
+        [InlineData("./input/day3.txt", 537732)]
+        public void Day3Part1(string input, int expected)
+        {
+            string[] lines = File.ReadAllLines(input);
+
+            int result = 0;
+            int lineIndex = -1;
+
+            int NoParts = 0;
+            int NoNotParts = 0;
+            
+            foreach (var line in lines)
+            {
+                lineIndex++;
+
+                int startPosition = -1;
+                int index = -1;
+                bool isPart = false;
+                foreach (char character in line)
+                {
+                    index++;
+                    if (Is0to9(character))
+                    {
+                        if (!isPart)
+                        {
+                            isPart = CheckForNeighbouringSymbols(lines, lineIndex, index);
+                        }
+
+                        if (startPosition == -1)
+                        {
+                            startPosition = index;
+                        }
+                    }
+                    else
+                    {
+                        if (startPosition > -1 && isPart)
+                        {
+                            var number = int.Parse(line.Substring(startPosition, index - startPosition));
+                            result += number;
+
+                            NoParts++;
+
+                        }
+                        else if(startPosition > -1)
+                        {
+                            NoNotParts++;
+                        }
+
+                        //reset
+                        startPosition = -1;
+                        isPart = false;
+                    }
+                }
+
+                //if number reaches the end of the line
+                if (startPosition > -1 && isPart)
+                {
+                    var number = int.Parse(line.Substring(startPosition, index+1 - startPosition));
+                    result += number;
+                }
+            }
+
+            //count parts
+            string inputText = File.ReadAllText(input);
+            Regex rgx = new Regex("[^0-9][0-9]+[^0-9]");
+            var matches = rgx.Matches(inputText);
+
+            _log.WriteLine($"parts: {NoParts} not: {NoNotParts} count: {matches.Count}");
+            Assert.Equal(expected, result);
+        }
+
+        private bool CheckForNeighbouringSymbols(string[] lines, int index, int position)
+        {
+            return
+
+            //topleft
+            HasSymbol(lines, index - 1, position - 1)
+
+            //top
+
+            || HasSymbol(lines, index - 1, position)
+
+            //bottomleft
+
+            || HasSymbol(lines, index + 1, position - 1)
+
+            //bottom
+
+            || HasSymbol(lines, index + 1, position)
+
+            //left 
+
+            || HasSymbol(lines, index, position - 1)
+
+            //top right
+
+            || HasSymbol(lines, index - 1, position + 1)
+
+            //bottomright
+
+            || HasSymbol(lines, index + 1, position + 1)
+
+            //right
+
+            || HasSymbol(lines, index, position + 1);
+
+        }
+
+        private bool HasSymbol(string[] lines, int index, int position)
+        {
+            if (index > 0 && position > 0 && index < lines.Length && position < lines[0].Length)
+            {
+                var ret = !Char.IsLetterOrDigit(lines[index][position]) && lines[index][position] != '.';
+                return ret;
+            }
+
+            return false;
+        }
+
+        private bool Is0to9(char character)
+        {
+            return character >= '0' && character <= '9';
         }
 
     }
