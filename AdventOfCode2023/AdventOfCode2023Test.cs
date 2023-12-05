1182e2dd2d445c225a6d4139b34fcb8d198a86e8
Fiorenzo Rutschmann <fiorenzo.rutschmann@otago.ac.nz>
2023-12-06 00:05:42 +1300

    Day 4 Part 2

--- AdventOfCode2023/AdventOfCode2023Test.cs
+++ AdventOfCode2023/AdventOfCode2023Test.cs
@@ -455,5 +455,52 @@ namespace AdventOfCode2023
 
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("./input/day4test.txt", 30)]
+        [InlineData("./input/day4.txt", 5095824)]
+        public void Day4Part2(string input, int expected)
+        {
+            string[] lines = File.ReadAllLines(input);
+            int[] copies = new int[lines.Length];
+            
+            //populate original cards
+            for (int i = 0; i < copies.Length; i++)
+            {
+                copies[i] = 1;
+            }
+
+            int result = 0;
+
+            for (int index = 0; index < lines.Length; index++)
+            {
+                string line = lines[index].Replace("  ", " ");
+                int startIndex = line.IndexOf(':');
+                int middleIndex = line.IndexOf('|');
+
+                var lottoNumbers = line[(startIndex + 1)..middleIndex].Split(" ").Where(n => n.Length > 0).Select(n => int.Parse(n)).ToList();
+                var numbers = line[(middleIndex + 1)..].Split(" ").Where(n => n.Length > 0).Select(n => int.Parse(n)).ToList();
+
+                int count = 0;
+                foreach (var number in numbers)
+                {
+                    if (lottoNumbers.Contains(number))
+                    {
+                        count++;
+                    }
+                }
+
+                for (int i = index+1; i < copies.Length && i < index+count+1 ; i++)
+                {
+                    copies[i] += copies[index];
+                }
+            }
+
+            result = copies.Sum();
+
+            Assert.Equal(expected, result);
+        }
+
+
     }
 }
