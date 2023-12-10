19de3002117ee30b2f05d22cdd2d7a8a7c35e64c
Fiorenzo Rutschmann <fiorenzo.rutschmann@otago.ac.nz>
2023-12-10 20:52:56 +1300

    Day 9 Part 2

--- AdventOfCode2023/days/Day9.cs
+++ AdventOfCode2023/days/Day9.cs
@@ -47,11 +47,49 @@ namespace AdventOfCode2023
         }
 
         [Theory]
-        [InlineData("./test/day9.txt", 0)]
-        [InlineData("./input/day9.txt", 0)]
+        [InlineData("./test/day9.txt", 2)]
+        [InlineData("./input/day9.txt", 1041)]
         public void Day9Part2(string input, long expected)
         {
             long result = 0;
+
+            string[] lines = File.ReadAllLines(input);
+
+            foreach (var line in lines)
+            {
+                List<List<int>> triangle = new List<List<int>>();
+
+                var triangleLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+
+                List<int> firsts = new List<int>();
+
+                //generate lines
+                while (!triangleLine.All(a => a == 0))
+                {
+                    firsts.Add(triangleLine.First());
+
+                    List<int> nextLine = new List<int>();
+                    
+                    int last = triangleLine.First();
+                    foreach (int i in triangleLine.Skip(1))
+                    {
+                        nextLine.Add(i - last);
+                        last = i;
+                    }
+
+                    
+                    triangleLine = nextLine;
+                }
+
+                int lastFirstNumber = 0;
+                firsts.Reverse();
+                foreach (int i in firsts)
+                {
+                    lastFirstNumber = i - lastFirstNumber;
+                }
+
+                result += lastFirstNumber;
+            }
 
             Assert.Equal(expected, result);
         }
